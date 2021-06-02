cac67b5ceaf74f38a17fa7f47318dac302a33e12
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-06-02 11:09:53 -0400

    Remove some comments from app.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,12 @@ import Header from "./components/Header"
 
 function App() {
   const colors = ['blue','pink','yellow']
-  const mappedHeaders = colors.map((color) => {return <Header color={color} title="Goodbye World!" />} )
+  const mappedHeaders = colors.map((color) => {return <Header color={color} title="Hi World!" />} )
 
 
   return (
       <div className="App">
-        {/* MAPPED */}
-        {/* [Header COMPonent, Header , Header, Header] */}
         {mappedHeaders}
-        
-        
-        
-        {/* <Header color="red" title="Goodbye World!" />
-        <Header color="blue" title="Goodmorning" />
-        <Header color="yellow" title="eri" />
-        <Header color="pink" title="pinkish" /> */}
-        
-        {/* Abstracted away the some magic from react */}
-        {/* { new Header({color: "blue", title: "Weird way of doing it"}).render()} */}
-        {/* {Header({color: "blue", title: "Weird way of doing it"})} */}
       </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,7 @@ import React from 'react'
 // }
 
 class Header extends React.Component{
-    constructor(props){
-        super(props)
-    }
+
 
     render(){
         return(
