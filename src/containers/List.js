8391d33349fed65302e76b1998f3a8f32cd85cef
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-06-03 12:50:36 -0400

    Finish React Forms Lecture

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import ListItem from "../components/Listitem"
 
 class List extends Component {
 
+
     render() {
+        // we have an array of blogs
+        // WE want to pass down one blogs info for each LI
+        // const blogs = this.props.blogs.map((blog) => <ListItem title={blog.title} content={blog.content}/>)
+        const blogs = this.props.blogs.map((blog) => <ListItem {...blog} />)
+
         return (
             <div>
-                <p> THE COLOR OF THE HEADER IS {this.props.color.toUpperCase()}</p>
+                <h3>BLOGS:</h3>
+                {blogs}
             </div>
         )
     }
